2075988fb78143e0afe90d29247a4b57153754cc
Vitor Machado <vitor.p.mach@gmail.com>
2015-11-10 20:57:24 -0200

    Persistence TODOs.

--- src/model/Persistence.java
+++ src/model/Persistence.java
@@ -1,10 +1,33 @@
 package model;
 
+import org.joda.time.DateTime;
+
+import java.io.File;
+
 /**
  * Created by Vitor on 10/11/2015.
  */
 public class Persistence {
 
-	//TODO: Implement Persistence.
+	private File persistenceFile;
+
+	public Persistence(String persistenceFilePath) {
+
+		persistenceFile = new File(persistenceFilePath);
+
+	}
+
+	public DateTime fetchDate() {
+
+		//TODO: Implement date fetching from file.
+
+		return null;
+	}
+
+	public void persistDate(DateTime date) {
+
+		//TODO: Implement date writing to file.
+
+	}
 
 }
