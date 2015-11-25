a8fffde3fde1ae45527ac868fcd7252690b22dd2
Vitor Machado <vitor.p.mach@gmail.com>
2015-11-25 21:21:13 -0200

    Starting generateXml implementation.

--- src/model/XMLWriter.java
+++ src/model/XMLWriter.java
@@ -8,6 +8,7 @@ import nu.xom.Element;
 public class XMLWriter {
 
 	public static final String ENVIAR_LOTE_RPS_ENVIO = "EnviarLoteRpsEnvio";
+	public static final String LOTE_RPS = "LoteRps";
 
 	private XMLWriter() {}
 
@@ -15,7 +16,10 @@ public class XMLWriter {
 
 		Element enviarLoteRpsEnvio = new Element(ENVIAR_LOTE_RPS_ENVIO);
 
+		Element loteRps = new Element(LOTE_RPS);
+
 		//TODO: Implement the rest of the XML generator.
+		enviarLoteRpsEnvio.appendChild(loteRps);
 
 	}
 }
