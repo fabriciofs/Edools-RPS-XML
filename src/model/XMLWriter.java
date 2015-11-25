a7b64d498d2e552913a8dfa1e5e03ebef06115dd
Vitor Machado <vitor.p.mach@gmail.com>
2015-11-25 21:24:43 -0200

    Starting generateXml implementation.

--- src/model/XMLWriter.java
+++ src/model/XMLWriter.java
@@ -1,6 +1,10 @@
 package model;
 
+import nu.xom.Document;
 import nu.xom.Element;
+import nu.xom.Serializer;
+
+import java.io.*;
 
 /**
  * Created by Vitor on 05/11/2015.
@@ -12,14 +16,21 @@ public class XMLWriter {
 
 	private XMLWriter() {}
 
-	public static void generateXML(RPSBulk rpsBulk, String path) {
+	public static void generateXML(RPSBulk rpsBulk, String path) throws IOException {
 
 		Element enviarLoteRpsEnvio = new Element(ENVIAR_LOTE_RPS_ENVIO);
 
 		Element loteRps = new Element(LOTE_RPS);
 
 		//TODO: Implement the rest of the XML generator.
+
 		enviarLoteRpsEnvio.appendChild(loteRps);
 
+		FileOutputStream fos = new FileOutputStream(path, false);
+		Document doc = new Document(enviarLoteRpsEnvio);
+		Serializer serializer = new Serializer(fos, "ISO-8859-1");
+		serializer.setIndent(4);
+		serializer.setMaxLength(64);
+		serializer.write(doc);
 	}
 }
