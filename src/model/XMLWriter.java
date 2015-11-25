6b5aadead62480c72dcac446322b65add3ed7f15
Vitor Machado <vitor.p.mach@gmail.com>
2015-11-25 21:12:44 -0200

    Starting generateXml implementation.

--- src/model/XMLWriter.java
+++ src/model/XMLWriter.java
@@ -1,15 +1,21 @@
 package model;
 
+import nu.xom.Element;
+
 /**
  * Created by Vitor on 05/11/2015.
  */
 public class XMLWriter {
 
+	public static final String ENVIAR_LOTE_RPS_ENVIO = "EnviarLoteRpsEnvio";
+
 	private XMLWriter() {}
 
 	public static void generateXML(RPSBulk rpsBulk, String path) {
 
-		//TODO: generateXML method.
+		Element enviarLoteRpsEnvio = new Element(ENVIAR_LOTE_RPS_ENVIO);
+
+		//TODO: Implement the rest of the XML generator.
 
 	}
 }
